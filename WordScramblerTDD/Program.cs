a36e94219248b7b998e9c3299d2d83439a0af60a
vhking <vidarheltne@hotmail.no>
2018-01-19 21:05:30 +0100

    Initial commit

--- WordScramblerTDD/Program.cs
+++ WordScramblerTDD/Program.cs
@@ -4,9 +4,53 @@ namespace WordScramblerTDD
 {
     class Program
     {
+        private static readonly FileReader _fileReader = new FileReader();
+        private static readonly WordMatcher _wordMatcher = new WordMatcher();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                bool continueWordScrambler = true;
+                do
+                {
+                    
+               
+                Console.Write(Constants.OptionsToEnterScrambledWords);
+                var options = Console.ReadLine() ?? string.Empty;
+
+                switch (options.ToUpper())
+                {
+                    case Constants.File:
+                        Console.Write(Constants.EnterScrambledWordsWithFile);
+                        ExecuteScrambledWordsInFile();
+                        break;
+                    case Constants.Manual:
+                        Console.Write(Constants.EnterScrambledWords);
+                        EcexuteScrambledWords();
+                        break;                    
+                    default:
+                        Console.WriteLine(Constants.EnterScrambledWordsNotRecognized);
+                        break;
+                }
+
+                var continueWordDecision = string.Empty;
+                do
+                {
+                    Console.WriteLine(Constants.OptionOnContinuingTheProgram);
+                    continueWordDecision = (Console.ReadLine() ?? string.Empty);
+                }while(
+                    !continueWordDecision.Equals(Constants.Yes, StringComparison.OrdinalIgnoreCase) &&
+                    !continueWordDecision.Equals(Constants.No, StringComparison.OrdinalIgnoreCase));
+
+                    continueWordScrambler = continueWordDecision.Equals(Constants.Yes, StringComparison.OrdinalIgnoreCase);
+                }while(continueWordScrambler); 
+            }
+            catch (System.Exception ex)
+            {
+                
+                Console.WriteLine(Constants.ErrorProgramWillBeTerminated + ex.Message);
+            }
+          
         }
     }
 }
