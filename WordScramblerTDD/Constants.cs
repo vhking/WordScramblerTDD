6a88b8861bbeb9ba9c9958334103d53fd2803550
vhking <vidarheltne@hotmail.no>
2018-01-20 11:26:45 +0100

    string const used to inform the user

--- WordScramblerTDD/Constants.cs
+++ WordScramblerTDD/Constants.cs
@@ -2,14 +2,14 @@ namespace WordScramblerTDD
 {
     class Constants
     {
-        public const string OptionsToEnterScrambledWords = "";
-        public const string EnterScrambledWordsWithFile = "";
-        public const string EnterScrambledWords = "";
-        public const string EnterScrambledWordsNotRecognized ="";
-        public const string OptionOnContinuingTheProgram ="";
-        public const string Yes = "";
-        public const string No = "";
-        public const string ErrorProgramWillBeTerminated = "";
+        public const string OptionsToEnterScrambledWords = "Enter word(s) manually or as a file: M - manual/F - file";
+        public const string EnterScrambledWordsWithFile = "Enter the path including the name of the file: ";
+        public const string EnterScrambledWords = "Enter word(s) to unscramble (Seperated by comma if multiple): ";
+        public const string EnterScrambledWordsNotRecognized ="Command not recongnized. Enter either M for manual or F for file";
+        public const string OptionOnContinuingTheProgram ="Would you like to continue unscramble words? Y/N";
+        public const string Yes = "Y";
+        public const string No = "N";
+        public const string ErrorProgramWillBeTerminated = "Error. The program will be terminated";
         public const string File = "F";
         public const string Manual = "M";
     }
